8c85f03864d9d7a14ae763d63d6ef949c3cf296d
umairnadeem123 <nadeem.umair123@gmail.com>
2019-08-18 21:49:23 -0700

    Clean up LineGraph component

--- src/components/LineGraph/index.jsx
+++ src/components/LineGraph/index.jsx
@@ -8,17 +8,9 @@ class LineGraph extends Component {
 
   render() { 
     return (
-      // <div style={{ width: "400px", height: "400px"}}>
-      //   <svg preserveAspectRatio="none">
-      //     <path stroke="black" stroke-width="2" d="M4 16.79 L 4 16.79 L 100 16.19" fill="none">
-      //     </path>
-      //   </svg>
-      // </div>
-    // <div style={{width: "400px", height:"300px", border: "1px solid black" }}>
-      <svg style={{width:"200px", height: "300px" }} viewBox="0 0 340.156 340.152" preserveAspectRatio="none">
+      <svg style={{width:"450px", height: "300px" }} viewBox="0 0 340.156 340.152" preserveAspectRatio="none">
         <path stroke="#000000" fill="none" strokeWidth="5" d="M100,100 c 0,93.926 -76.146,170.074 200,100"> </path>
       </svg>
-    // </div>
     );
   }
 }
