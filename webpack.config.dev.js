2ae3175dc5e0ffafefe60a6517783937d0ca4c11
umairnadeem123 <nadeem.umair123@gmail.com>
2019-08-18 18:41:13 -0700

    Initialize React setup

---
+++ webpack.config.dev.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve('src'),
+  output: {
+    path: path.resolve('lib'),
+    filename: 'Component.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  devServer: {
+    contentBase: path.resolve('lib'),
+    compress: true,
+    port: process.env.PORT || 3000,
+  },
+};
