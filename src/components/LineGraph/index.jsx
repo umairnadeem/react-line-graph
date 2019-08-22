d86f00433429c6fa7a270d940b8e922b5e473d59
umairnadeem123 <nadeem.umair123@gmail.com>
2019-08-21 21:39:47 -0700

    Add findSlope method

--- src/components/LineGraph/index.jsx
+++ src/components/LineGraph/index.jsx
@@ -12,7 +12,7 @@ class LineGraph extends Component {
     return (
       <svg
         style={{ width: '100%', height: '100%' }}
-        viewBox="0 0 100 100"
+        viewBox="0 0 1000 1000"
         preserveAspectRatio="none"
       >
         {/* <path
@@ -21,24 +21,15 @@ class LineGraph extends Component {
           strokeWidth="1"
           d={`M0,100 ${toBezier('0,100', '45,50', 25)} ${toBezier('45,50', '90,100', -25)}`}
         /> */}
-        <path
-          stroke="#000000"
-          fill="none"
-          strokeWidth="1"
-          d={`M0,100 ${toBezier('0,100', '22.5,75', -5)} ${toBezier('22.5,75', '45,50', 5)} ${toBezier('45,50', '67.5,75', -5)}${toBezier('67.5,75', '90,100', 5)}`}
-        />
         {/* <path
           stroke="#000000"
           fill="none"
           strokeWidth="1"
-          d={`M0,100 ${toBezier('0,100', '45,50', 10)}`}
+          d={`M0,100 ${toBezier('0,100', '22.5,75', 5)} ${toBezier('22.5,75', '45,50', 5)} ${toBezier('45,50', '67.5,75', 0)}${toBezier('67.5,75', '90,100', 5)}`}
         /> */}
         <g>
-          <circle cx="0" cy="100" r="2" fill="red" />
-          <circle cx="22.5" cy="75" r="2" fill="red" />
-          <circle cx="45" cy="50" r="2" fill="red" />
-          <circle cx="67.5" cy="75" r="2" fill="red" />
-          <circle cx="90" cy="100" r="2" fill="red" />
+          <circle cx="100" cy="200" r="2" fill="red" />
+          <circle cx="200" cy="100" r="2" fill="red" />
         </g>
       </svg>
     );
--- src/components/_helpers/toBezier.js
+++ src/components/_helpers/toBezier.js
@@ -5,23 +5,32 @@
  * @param {Integer} amount - Amount of Bezier from 0 to 100
  */
 export const toBezier = (from, to, amount) => {
-  // "M0,50 Q45,20 90,10"
-  const parse = (str) => str
-    .trim()
-    .split(',')
-    .map((x) => parseInt(x, 10));
 
-  const a = parse(from);
-  const b = parse(to);
-  const slope = (b[1] - a[1]) / (b[0] - a[0]);
+
+
   const intercept = a[1] - slope * a[0];
   const midX = a[0] + (b[0] - a[0]) / 2;
   const midY = slope * (midX) + intercept;
   const perpSlope = -1 * (1 / slope);
   const perpIntercept = midY - perpSlope * midX;
-  console.log(`y = ${slope}x + ${intercept}`)
-  console.log(`y = ${perpSlope}x + ${perpIntercept}`)
   const outputX = midX - amount;
   const outputY = perpSlope * outputX + perpIntercept;
   return `S${outputX},${outputY} ${b.join(',')}`;
+};
+
+export const getSlope = (from, to) => {
+  const parse = (str) => str
+    .trim()
+    .split(',')
+    .map((x) => parseInt(x, 10));
+  const a = parse(from);
+  const b = parse(to);
+  const slope = (b[1] - a[1]) / (b[0] - a[0]);
+  return slope;
+};
+
+export const findCtrlPoint = (x, y, z, amount = 0) => {
+  if (typeof x !== 'string' || typeof y !== 'string' || typeof z !== 'string') {
+    throw new Error('Coordinates must be of type string.');
+  }
 };
