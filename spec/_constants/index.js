e19d35e753b5a0c35a5dea9cb646b968f38c53ca
umairnadeem123 <nadeem.umair123@gmail.com>
2019-08-28 15:54:04 -0700

    Refactor App component with React Hooks

--- spec/_constants/index.js
+++ spec/_constants/index.js
@@ -3,4 +3,11 @@ module.exports = {
   pointsWithNegatives: [[0, -10], [-10, -70], [20, -45], [-30, 100], [40, -10], [-60, 80]],
   pointsWithDecimals: [[0.001, -10.92], [-10, -7230.87], [20.2100, -45.00000]],
   pointsUnordered: [[20, 45], [0, 10], [40, 10], [10, 70], [60, 80], [30, 100]],
+  pointsRandom: (() => {
+    const data = [];
+    for (let i = 0; i < 100; i += 1) {
+      data.push([i, Math.random() * 20]);
+    }
+    return data;
+  })(),
 };
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,58 +1,27 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import LineGraph from './LineGraph';
+import points from '../../spec/_constants';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-      data: [],
-    };
-  }
+const App = () => {
+  const [hovered, onHover] = useState([0, 0]);
+  const data = points.pointsRandom; // Get random set of points
+  const props = {
+    data,
+    smoothing: 0.4,
+    hover: true,
+    accent: 'rgb(73,88,209)',
+    fillBelow: 'url(#grad1)',
+    strokeWidth: 3,
+    onHover,
+    compression: 0.5,
+  };
 
-  componentDidMount() {
-    // const data = [[0, 10], [10, 70], [20, 45], [30, 100], [40, 10], [60, 80]];
-    // const data = [[0, 100], [100, 50]];
-    const data = [];
-    for (let i = 0; i < 100; i += 1) {
-      data.push([i, Math.random() * 20]);
-    }
-    this.setState({ data });
-  }
-
-  handleHover(coordinates) {
-    this.setState({ value: coordinates[1] });
-  }
-
-  render() {
-    const { data, value } = this.state;
-    const props = {
-      data,
-      smoothing: 0.4,
-      hover: true,
-      accent: 'rgb(73,88,209)',
-      fillBelow: 'url(#grad1)',
-      strokeWidth: 3,
-      onHover: this.handleHover.bind(this),
-      compression: 0.5,
-    };
-
-    return (
-      <div id="test">
-        Some text...
-        <h1 id="logo">{Number(value).toFixed(2)}</h1>
-        <svg width="0" height="0">
-          <defs>
-            <linearGradient id="grad1" x1="0%" y1="0%" x2="0%" y2="100%">
-              <stop offset="0%" style={{ stopColor: 'rgb(132,144,235)', stopOpacity: 1 }} />
-              <stop offset="100%" style={{ stopColor: 'rgb(206,196, 235)', stopOpacity: 1 }} />
-            </linearGradient>
-          </defs>
-        </svg>
-        <LineGraph {...props} />
-      </div>
-    );
-  }
-}
+  return (
+    <div id="test">
+      <h1 id="logo">{Number(hovered[1]).toFixed(2)}</h1>
+      <LineGraph {...props} />
+    </div>
+  );
+};
 
 export default App;
