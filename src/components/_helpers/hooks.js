f1eab0f491dd11d302ec9a1e4a59a3b61103558e
umairnadeem123 <nadeem.umair123@gmail.com>
2019-09-01 17:21:47 -0700

    Remove useCallback in custom useMousePosition hook

--- src/components/_helpers/hooks.js
+++ src/components/_helpers/hooks.js
@@ -1,14 +1,14 @@
-import { useState, useCallback } from 'react';
+import { useState } from 'react';
 import { getMousePosition } from './dom';
 
 export const useMousePosition = (initialState = [0, 0], calcWidth, calcHeight) => {
   const [position, changePosition] = useState(initialState);
-  const setPosition = useCallback((e) => { // TODO: useCallback unnecessary?
+  const setPosition = (e) => {
     const [x, y] = getMousePosition(e, calcWidth, calcHeight);
     if (x <= calcWidth && y <= calcHeight) {
       changePosition([x, y]);
     } else changePosition([-1 * calcWidth, -1 * calcHeight]);
-  });
+  };
 
   return [position, setPosition];
 };
